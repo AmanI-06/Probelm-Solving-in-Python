d278fbad9782b42d32f588a3ddf0f57014d310a6
Amani <amani06001@gmail.com>
2020-09-06 12:22:55 +0530

    commit on master

---
+++ number of vowels in String.py
@@ -0,0 +1,8 @@
+print('hello')
+string=input("Enter string:")
+Count=0
+for i in string:
+      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+            Count+=1
+print("Number of vowels are:")
+print(Count)
