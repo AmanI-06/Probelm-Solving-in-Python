97a2132f1110da0464749144e8f91daf9523ba49
AmanI-06 <51697919+AmanI-06@users.noreply.github.com>
2020-09-06 12:25:15 +0530

    Delete 1.py

--- 1.py
+++
@@ -1,8 +0,0 @@
-print('hello')
-string=input("Enter string:")
-Count=0
-for i in string:
-      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
-            Count+=1
-print("Number of vowels are:")
-print(Count)
